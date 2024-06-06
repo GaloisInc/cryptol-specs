bea3dc8c2f1adb31b905f7bbea9277f8a4195b52
Marcella Hastings <marcella@galois.com>
2024-06-06 09:22:12 -0400

    aes-gcm: fix decryption bug, add properties #46
    - Adds APIs to match the NIST spec
    - Adds properties of API equivalence
    - Adds properties that test decryption bug
    - Adds a batch file

---
+++ Primitive/Symmetric/Cipher/Authenticated/aes_gcm.bat
@@ -0,0 +1,10 @@
+:l AES_GCM.cry
+
+:prove GCM_AE_test_vector_0
+:prove GCM_AE_test_vector_1
+:prove GCM_AE_test_vector_2
+:prove GCM_AE_test_vector_3
+:prove GCM_AE_test_vector_4
+:prove GCM_AE_invalid_test_vector
+
+// TODO: add equivalence properties (mult, enc, dec)
