1b9129892165bf0d67f7222f4e5d6423b3783003
Marcella Hastings <marcella@galois.com>
2024-06-17 14:25:32 -0400

    aes-gcm: clean up GCM and AES-GCM modules
    - separates private methods in GCM module
    - tries to make naming more canonical across properties
    - adds additional properties for AES-GCM
    - expands batch file and documentation for AES-GCM

--- Primitive/Symmetric/Cipher/Authenticated/aes_gcm.bat
+++ Primitive/Symmetric/Cipher/Authenticated/aes_gcm.bat
@@ -1,10 +1,26 @@
 :l AES_GCM.cry
 
-:prove GCM_AE_test_vector_0
-:prove GCM_AE_test_vector_1
-:prove GCM_AE_test_vector_2
-:prove GCM_AE_test_vector_3
-:prove GCM_AE_test_vector_4
-:prove GCM_AE_invalid_test_vector
+:prove AES_GCM_test_vector_0
+:prove AES_GCM_test_vector_1
+:prove AES_GCM_test_vector_2
+:prove AES_GCM_test_vector_3
+:prove AES_GCM_test_vector_4
+:prove AES_GCM_invalid_test_vector
 
-// TODO: add equivalence properties (mult, enc, dec)
+// The following checks do not really provide any significant formal
+// verification because they check so little of the sample space.
+
+// These properties can be checked manually; one of the APIs calls the other.
+// They take more than an hour to prove.
+:check decryptionApisAreEquivalent `{K=128, IV=96, AAD=0, T=128, P=256} {E=AES::encrypt}
+:check encryptionApisAreEquivalent `{K=128, IV=96, AAD=0, T=128, P=256} {E=AES::encrypt}
+
+// This property is independent of the type parameters but we have to specify
+// them anyway.
+// It takes more than 25 minutes to prove.
+:check dotAndMultAreEquivalent `{K=128, IV=96, AAD=0, T=128} {E=AES::encrypt}
+
+// Make sure that decryption is the inverse of encryption
+// This property takes more than 20 minutes to prove
+// It's also spot-checked in the test vectors
+:check aesGcmIsSymmetric
