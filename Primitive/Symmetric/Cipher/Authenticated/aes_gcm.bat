fb0a046e9f6b97ba0ede3c3f7d53387d6cdb2163
Marcella Hastings <marcella@galois.com>
2024-07-02 15:13:15 -0400

    aes-gcm: update to use cipher interface #78
    - modifies the GCM mode implementation to use the interface
    - replaces AES_GCM with separate instantiations of the GCM functor for
      our AES's of interest, and an independent test file
    -

--- Primitive/Symmetric/Cipher/Authenticated/aes_gcm.bat
+++ Primitive/Symmetric/Cipher/Authenticated/aes_gcm.bat
@@ -1,28 +1,35 @@
-:l AES_GCM.cry
+:l Tests/TestAES_GCM.cry
 
-:prove AES_GCM_test_vector_0
-:prove AES_GCM_test_vector_1
-:prove AES_GCM_test_vector_2
-:prove AES_GCM_test_vector_3
-:prove AES_GCM_test_vector_4
-:prove AES_GCM_invalid_test_vector
+:prove aes128_vector_0
+:prove aes128_vector_1
+:prove aes128_vector_2
+:prove aes128_vector_3
+
+:prove aes256_vector_0
+:prove aes256_invalid_vector_1
 
 // The following checks do not really provide any significant formal
 // verification because they check so little of the sample space.
 // They each take a long time to `:prove` and would likely require
 // manual modification to prove in a reasonable amount of time.
 
-// These properties can be checked manually; one of the APIs calls the other.
-// They take more than an hour to `:prove`.
-:check aesGcmDecryptionApisAreEquivalent
-:check aesGcmEncryptionApisAreEquivalent
 
-// This property is independent of the type parameters but we have to specify
-// them anyway.
-// It takes more than 25 minutes to `:prove`.
-:check dotAndMultAreEquivalent `{K=128, IV=96, AAD=0, T=128} {E=AES128::encrypt}
+:l Instantiations/AES128_GCM.cry
 
 // Make sure that decryption is the inverse of encryption
 // This property takes more than 20 minutes to `:prove`.
 // It's also spot-checked in the test vectors
-:check aesGcmIsSymmetric
+// Here, we just pick a fixed set of parameters, but it should be true
+// for all valid tag, aad, and plaintext lengths.
+// - P = 256 because we want to test the block chaining, so we need at least 2
+// - IV = 96 because it's the shortest allowable value
+// - AAD = 5 because we want to make sure it's incorporated
+:check gcmIsSymmetric `{AAD=5, P=256, IV=96}
+
+// This takes more than 25 minutes to `:prove`.
+:check dotAndMultAreEquivalent
+
+// Repeat the above checks for AES256
+:l Instantiations/AES256_GCM.cry
+:check gcmIsSymmetric `{AAD=5, P=256, IV=96}
+:check dotAndMultAreEquivalent
